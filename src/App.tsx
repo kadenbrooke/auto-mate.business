f90448fc200af294ee74a48b52dfa64507229bf7
kadenbrooke <kadenbrooke@gmail.com>
2025-02-28 16:21:54 -0600

    Update hook

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ function App() {
               <span className="block text-accent">With AI Solutions</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              Our AI tools will automate your systems and let you focus on what is most important: making money
+              Our AI tools will help you save time, cut costs, and increase revenue, so you focus on what is most important.
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
